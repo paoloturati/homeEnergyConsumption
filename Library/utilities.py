d7e9a5809cf270d9f2e959e4c7eea15757af5498
paolo <turatip@gmail.com>
2023-04-16 18:55:56 +0100

    fix-bug-dt: written ad hoc function to parse the date and added main function in utilities

--- Library/utilities.py
+++ Library/utilities.py
@@ -1,7 +1,57 @@
+import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 import requests
 
+
+def format_date(date_string, wanted_feature):
+    """
+    This method, given an input date string and a wanted feature,
+    returns either year, month, day, hour and minute.
+
+    For example, given this input "2023-04-16T00:30:00+01:00",
+    it will return the year as 2023, the month as 04, the day as 16,
+    the hour as 00 and the minute as 30.
+    """
+
+    if "T" not in date_string:
+        # date is in the wrong format
+        return np.nan
+
+    if wanted_feature in ["year", "month", "day"]:
+        date_part = date_string.split("T")[0]
+        try:
+            year = int(date_part.split("-")[0])
+            month = int(date_part.split("-")[1])
+            day = int(date_part.split("-")[2])
+        except:
+            # date part has the wrong format
+            year = np.nan
+            month = np.nan
+            day = np.nan
+        if wanted_feature == "year":
+            return year
+        elif wanted_feature == "month":
+            return month
+        else:
+            return day
+    elif wanted_feature in ["hour", "minute"]:
+        hour_part = date_string.split("T")[1]
+        try:
+            hour = int(hour_part.split(":")[0])
+            minute = int(hour_part.split(":")[1])
+        except:
+            # hour part has the wrong format
+            hour = np.nan
+            minute = np.nan
+        if wanted_feature == "hour":
+            return hour
+        else:
+            return minute
+    else:
+        # wanted feature not available
+        return np.nan
 
 class Utilities:
     def __init__(self, url_string, user_id):
@@ -40,22 +90,16 @@ class Utilities:
         retrieved data.
         """
 
-        self.results_df["interval_start"] = pd.to_datetime(
-            self.results_df["interval_start"]
-        )
-        self.results_df["interval_end"] = pd.to_datetime(
-            self.results_df["interval_end"]
-        )
-        self.results_df["year_start"] = self.results_df["interval_start"].dt.year
-        self.results_df["month_start"] = self.results_df["interval_start"].dt.month
-        self.results_df["day_start"] = self.results_df["interval_start"].dt.day
-        self.results_df["hour_start"] = self.results_df["interval_start"].dt.hour
-        self.results_df["minute_start"] = self.results_df["interval_start"].dt.minute
-        self.results_df["year_end"] = self.results_df["interval_end"].dt.year
-        self.results_df["month_end"] = self.results_df["interval_end"].dt.month
-        self.results_df["day_end"] = self.results_df["interval_end"].dt.day
-        self.results_df["hour_end"] = self.results_df["interval_end"].dt.hour
-        self.results_df["minute_end"] = self.results_df["interval_end"].dt.minute
+        self.results_df["year_start"] = self.results_df["interval_start"].apply(format_date, args=("year",))
+        self.results_df["month_start"] = self.results_df["interval_start"].apply(format_date, args=("month",))
+        self.results_df["day_start"] = self.results_df["interval_start"].apply(format_date, args=("day",))
+        self.results_df["hour_start"] = self.results_df["interval_start"].apply(format_date, args=("hour",))
+        self.results_df["minute_start"] = self.results_df["interval_start"].apply(format_date, args=("minute",))
+        self.results_df["year_end"] = self.results_df["interval_end"].apply(format_date, args=("year",))
+        self.results_df["month_end"] = self.results_df["interval_end"].apply(format_date, args=("month",))
+        self.results_df["day_end"] = self.results_df["interval_end"].apply(format_date, args=("day",))
+        self.results_df["hour_end"] = self.results_df["interval_end"].apply(format_date, args=("hour",))
+        self.results_df["minute_end"] = self.results_df["interval_end"].apply(format_date, args=("minute",))
 
     def check_daily_consumption(self, day_string):
 
@@ -104,3 +148,19 @@ class Utilities:
         plt.legend()
         plt.title(title)
         plt.show()
+
+
+if __name__ == "__main__":
+
+    # read credentials as environment variables
+    url_string_electricity = os.getenv("URL_ELECTRICITY_OCTOPUS")
+    user_id = os.getenv("USER_ID_OCTOPUS")
+
+    # initialise object
+    session = Utilities(url_string=url_string_electricity, user_id=user_id)
+
+    session.retrieve_readings()
+    session.parse_data()
+    session.extract_time_metrics()
+    # for testing purposes only
+    session.check_daily_consumption("2023-04-13")
